969dc23d3ddba30d8c1310a718a82c324dc97240
Lior Rozen <lior@win.com>
2015-08-08 14:56:17 +0300

    first usage of control view and control events

--- src/app.js
+++ src/app.js
@@ -3,14 +3,18 @@ define([
   'underscore',
   'backbone',
   'views/primary/primary',
+  'views/primary/control',
   'router',
-], function($, _, Backbone, PrimaryView, Router){
+], function($, _, Backbone, PrimaryView, ControlView, Router){
   var initialize = function(){
     Router.initialize();
 
-
+    var controlView = new ControlView();
     var primaryView = new PrimaryView();
     primaryView.render();
+
+    controlView.on('foo', primaryView.set_seed, primaryView);
+    controlView.on('tick', primaryView.tick, primaryView);
 
   };
 
