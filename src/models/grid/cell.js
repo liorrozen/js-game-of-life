f8ce3ebc464343eee0050de1690ea9f38cb06069
Lior Rozen <lior@win.com>
2015-08-08 14:54:12 +0300

    define attributes and set id value

--- src/models/grid/cell.js
+++ src/models/grid/cell.js
@@ -4,12 +4,15 @@ define([
     'backbone',
 ], function($, _, Backbone){
     var CellModel = Backbone.Model.extend({
+        id: null,
+        x: null,
+        y: null,
         live: false,
-
         initialize: function( attributes ){
             for ( var prop in attributes ){
                 this.set( prop, attributes[ prop ] );
             }
+            this.id = this.get('x') + '_' + this.get('y');
         }
     });
     return CellModel;
